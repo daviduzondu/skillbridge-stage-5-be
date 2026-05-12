c0c6fe8414020f184bb33830f10a5d937413dfa8
David Uzondu <uzondudavid2004@gmail.com>
2026-05-12 21:47:45 +0100

    fix tsdown.config.ts

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
-  entry: "./src/main.ts",
+  entry: ["./src/main.ts", "./src/database/**/*.ts"],
   format: "cjs",
   outDir: "./dist",
   clean: true,
   unbundle: true,
-  failOnWarn: true,
 });
