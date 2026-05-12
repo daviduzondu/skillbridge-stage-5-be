499e8b030062262bffb61af9cdff462dfb4f64e4
David Uzondu <uzondudavid2004@gmail.com>
2026-05-12 22:53:46 +0100

    switch to .cjs

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsdown";
 
 export default defineConfig({
   entry: ["./src/main.ts", "./src/database/**/*.ts"],
-  format: "esm",
+  format: "cjs",
   outDir: "./dist",
   clean: true,
   unbundle: true,
