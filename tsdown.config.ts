3dedc97b26c266cef0dd827cce1c3f5f0a422924
David Uzondu <uzondudavid2004@gmail.com>
2026-05-12 22:38:08 +0100

    one last try i guess

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsdown";
 
 export default defineConfig({
   entry: ["./src/main.ts", "./src/database/**/*.ts"],
-  format: "cjs",
+  format: "esm",
   outDir: "./dist",
   clean: true,
   unbundle: true,
